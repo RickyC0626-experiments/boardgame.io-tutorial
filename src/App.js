3b4e2570cd5446c924568cfdc15103c7dbf9acb2
Ricky Chon <rickychon99@gmail.com>
2021-11-29 01:04:29 -0500

    Add local multiplayer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import React from 'react';
 import { Client } from 'boardgame.io/react';
+import { Local } from 'boardgame.io/multiplayer';
 import { TicTacToe } from './Game';
 import { TicTacToeBoard } from './Board';
 
-const App = Client({
+const TicTacToeClient = Client({
     game: TicTacToe,
     board: TicTacToeBoard,
+    multiplayer: Local({
+        persist: true
+    }),
 });
+
+const App = () => (
+    <div>
+        <TicTacToeClient playerID="0" />
+        <TicTacToeClient playerID="1" />
+    </div>
+);
 
 export default App;
