9743ad630f04eaa24860f8cfd9a7b71bd51b8dfe
Ricky Chon <rickychon99@gmail.com>
2021-11-29 02:40:45 -0500

    Add remote master w/ server setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Client } from 'boardgame.io/react';
-import { Local } from 'boardgame.io/multiplayer';
+import { SocketIO } from 'boardgame.io/multiplayer';
 import { TicTacToe } from './Game';
 import { TicTacToeBoard } from './Board';
 
 const TicTacToeClient = Client({
     game: TicTacToe,
     board: TicTacToeBoard,
-    multiplayer: Local({
-        persist: true
+    multiplayer: SocketIO({
+        server: 'localhost:8000'
     }),
 });
 
-const App = () => (
-    <div>
-        <TicTacToeClient playerID="0" />
-        <TicTacToeClient playerID="1" />
-    </div>
-);
+const App = () => {
+    const [playerID, setPlayerID] = useState(null);
+
+    return playerID === null ? (
+        <div>
+            <p>Play as</p>
+            <button onClick={() => setPlayerID("0")}>
+                Player 0
+            </button>
+            <button onClick={() => setPlayerID("1")}>
+                Player 1
+            </button>
+        </div>
+    ) : (
+        <div>
+            <TicTacToeClient playerID={playerID} />
+        </div>
+    );
+};
 
 export default App;
